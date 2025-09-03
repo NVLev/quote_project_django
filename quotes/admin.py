09fe211525798d13f397dbf9b6e2be53868d66b7
natalia_levant <an_smir@mail.ru>
2025-09-03 22:47:37 +0300

    added models to admin panel

--- quotes/admin.py
+++ quotes/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Source, Quote
 
-# Register your models here.
+
+admin.site.register(Source)
+
+
+
+@admin.register(Quote)
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = ('text', 'source', 'weight', 'view_count', 'likes', 'dislikes')
+
+    search_fields = ('text',)
+
+    list_filter = ('source', 'weight')
+
+    fieldsets = (
+        (None, {
+            'fields': ('text', 'source')
+        }),
+        ('Статистика и вес', {
+            'fields': ('weight', 'view_count', 'likes', 'dislikes'),
+            'classes': ('collapse',)
+        }),
+    )
