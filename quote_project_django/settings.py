e506eacdf9c4cd069acee6d7b6673063f6f5559d
natalia_levant <an_smir@mail.ru>
2025-09-07 20:13:18 +0300

    minor changes in secret key processing

--- quote_project_django/settings.py
+++ quote_project_django/settings.py
@@ -21,6 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
