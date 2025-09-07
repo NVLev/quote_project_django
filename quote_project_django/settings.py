008780587e5b8b1d90d489b136b541f5d462b5e0
natalia_levant <an_smir@mail.ru>
2025-09-07 20:13:18 +0300

    minor changes in secret key processing

--- quote_project_django/settings.py
+++ quote_project_django/settings.py
@@ -21,20 +21,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['NVLev.pythonanywhere.com', 'localhost']
+ALLOWED_HOSTS = ['nvlev.pythonanywhere.com', 'localhost']
 
-if 'PYTHONANYWHERE_DOMAIN' in os.environ:
-    DEBUG = False
-    ALLOWED_HOSTS = [os.environ['PYTHONANYWHERE_DOMAIN']]
-else:
-    DEBUG = True
-    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+# if 'PYTHONANYWHERE_DOMAIN' in os.environ:
+#     DEBUG = False
+# else:
+#     DEBUG = True
+#     ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
